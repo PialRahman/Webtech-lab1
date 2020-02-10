33c2aedee395fe6e3e15f7c6e07c88dc3b2fe985
PialRahman <58075858+PialRahman@users.noreply.github.com>
2020-02-10 13:47:08 +0600

    5

---
+++ lab3task5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Degree:</h1>
+
+<form action="">
+  <input type="checkbox" name="vehicle1" value="Bike">
+  <label for="vehicle1"> SSC</label>
+  <input type="checkbox" name="vehicle2" value="Car">
+  <label for="vehicle2">HSC</label>
+   <input type="checkbox" name="vehicle2" value="Car">
+  <label for="vehicle2">BSc</label>
+   <input type="checkbox" name="vehicle2" value="Car">
+  <label for="vehicle2">MSc</label><br>
+  <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
