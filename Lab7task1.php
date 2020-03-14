1cccee30052e0162deab87ef7e05877e07f583cb
PialRahman <58075858+PialRahman@users.noreply.github.com>
2020-03-14 14:54:01 +0600

    lab 7

---
+++ Lab7task1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<img src="Company.PNG" alt="company logo" height="70" weight="70">
+<p align="right"><a href="Lab7task1.php">Home |</a>
+<a href="Lab7task3.php">Log in |</a>
+<a href="Lab7task2.php">Registration |</a></p>
+<hr>
+<h1>Welcome to xCompany
+	<br><br><br><br><br>
+
+
+
+
+
+
+
+
+<hr>
+</body>
+</html>
