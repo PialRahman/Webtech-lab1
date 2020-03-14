f3ccffc7b0fc38b694401d57ba0bdeb20796749c
PialRahman <58075858+PialRahman@users.noreply.github.com>
2020-03-14 14:54:51 +0600

    Add files via upload

---
+++ Lab7task3.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<img src="Company.PNG" alt="company logo" height="70" weight="70">
+<p align="right"><a href="Lab7task1.php">Home |</a>
+<a href="Lab7task3.php">Log in |</a>
+<a href="Lab7task2.php">Registration |</a></p>
+<hr>
+<h1>LOGIN</h1>
+	  User Name: <input type="text" name="Username">
+  <br><br>
+    Password: <input type="text" name="Password">
+  <br><br>
+  <hr>
+   <input type="checkbox" id="rememberme" name="rememberme" value="save">
+  <label for="vehicle1">Remember me</label><br>
+
+<input type="submit" name="submit" value="Submit"><a href="Lab7task4.php">Forgor Password?</a>
+
+
+
+
+
+
+<hr>
+</body>
+</html>
